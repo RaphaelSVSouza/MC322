7ded7a8461f15a3caf52238c3def55fb73834625
RaphaelSVSouza <raphael.sv.souza@gmail.com>
2023-08-15 17:33:51 -0300

    terminar monografia

---
+++ lab01/src/lab01/Monografia.java
@@ -0,0 +1,13 @@
+package lab01;
+
+public class Monografia extends Obra{
+    private final String reflexao; // foco do tema escolhido para a monografia
+    private final String chamada;
+
+
+    public Monografia(String reflexao, String chamada) {
+        this.reflexao = reflexao;
+        this.chamada = chamada;
+    }
+
+}
