2ae60e88745ddf245d1c3ba21ec8ef21e728c8fc
RaphaelSVSouza <raphael.sv.souza@gmail.com>
2023-08-15 17:32:40 -0300

    atualização 1

--- lab01/src/lab01/Main.java
+++ lab01/src/lab01/Main.java
@@ -5,13 +5,10 @@ public class Main {
 	public static void main(String[] args) {
 		Biblioteca biblioteca = new Biblioteca(100);
 
-		Livro livro1 = new Livro("Livro", "ROM", "9788574803999",
-		"Dom Casmurro", "Machado de Assis", "" /* vaga da universidade não preenchida */, 
-		2023, 2, 400, 10, false);
+        Livro livro1 = new Livro("Livro", "ROM", "9788574803999",
+                "Dom Casmurro", "Machado de Assis", 2023, 2, "Principis",
+                400, 10, 0, false);
 
-		Livro livro2 = new Livro("Artigo", "TEC", "6599012345678",
-		"Java POO", "Raphael Salles", "Unicamp", 2023, 0 /* sem edição para NÃO livros */,
-		 400, 0 /* sem exemplares físicos */, true);
 
 		Pessoa marcos = new Pessoa("Marcos", 51234714809L);
 		Professor prof1 = new Professor(marcos.getNome(), marcos.getCPF(), 123456, "IC");
